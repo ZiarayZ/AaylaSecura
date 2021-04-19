74ae918ed6c003f3aaece6b9cafd00cbdc26eebe
ZiarayZ <jakesmith@binaryrage.com>
2021-04-19 13:30:10 +0100

    Create user.java
    added password validation methods

---
+++ src/groupProject/user.java
@@ -0,0 +1,40 @@
+package groupProject;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.SecureRandom;
+
+public class user {
+
+    private static boolean validatePassword(String original, String stored) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //split stored hash apart
+        String[] parts = stored.split(":");
+        int iter = Integer.parseInt(parts[0]);
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+
+        //hash original using stored
+        PBEKeySpec keySpec = new PBEKeySpec(original.toCharArray(), salt, iter, hash.length*8);
+        SecretKeyFactory secKeyF = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = secKeyF.generateSecret(keySpec).getEncoded();
+        
+        //compare hashes
+        int diff = hash.length ^ testHash.length;
+        for (int i=0; i<hash.length && i<testHash.length; i++) {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException {
+        //convert from hex
+        byte[] bytes = new byte[hex.length()/2];
+        for (int i=0; i<bytes.length; i++) {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2*i, 2*i+2), 16);
+        }
+        return bytes;
+    }
+}
