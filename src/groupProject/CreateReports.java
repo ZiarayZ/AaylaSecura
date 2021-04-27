304ab1cd49a49f5fb951db95a0d157a610e49775
ZiarayZ <jakesmith@binaryrage.com>
2021-04-27 11:28:49 +0100

    Create CreateReports
    added simple attributes and method

---
+++ src/groupProject/CreateReports.java
@@ -0,0 +1,12 @@
+package groupProject;
+
+public class CreateReports {
+    private database tasksDB = new database();
+    private String taskStatus;
+    private String caretakerPerformance;
+    private String taskCompletionLevel;
+
+    public void generateReport() {
+
+    }
+}
