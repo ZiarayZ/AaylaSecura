6f1123a51f8651b76bf6811c97f65e4f55c663df
ZiarayZ <jakesmith@binaryrage.com>
2021-04-28 13:37:06 +0100

    quick fix

--- src/AaylaSecuraMain.java
+++ src/AaylaSecuraMain.java
@@ -3,19 +3,16 @@ import java.sql.SQLException;
 
 public class AaylaSecuraMain {
 	
-	static database db;
-	private UserManagement user;
-	private LogTasks logged_tasks;
+	static database db = new database();
+	private UserManagement user = new UserManagement(db);
+	private LogTasks logged_tasks = new LogTasks(db);
 
 	public static void  main(String[] args) throws SQLException{
-		db=new database();
 		database.addNewJob("test job");
 		ResultSet jobs = db.getAllJobs();
 		while(jobs.next()) {
 			System.out.println(jobs.getInt(1)+"|"+jobs.getString(2));
 		}
-		user = new UserManagement(db);
-		logged_tasks = new LogTasks(db);
 	}
 	
 	
