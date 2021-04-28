718ec95abf3958e274ac927ce19cbfbeae4bbe5c
ZiarayZ <jakesmith@binaryrage.com>
2021-04-28 14:40:46 +0100

    handling exceptions
    -imported 2 exceptions
    -added try and catch for testing other things

--- src/AaylaSecuraMain.java
+++ src/AaylaSecuraMain.java
@@ -1,3 +1,5 @@
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -7,11 +9,18 @@ public class AaylaSecuraMain {
 	private UserManagement user = new UserManagement(db);
 	private LogTasks logged_tasks = new LogTasks(db);
 
-	public static void  main(String[] args) throws SQLException{
-		database.addNewJob("test job");
-		ResultSet jobs = db.getAllJobs();
-		while(jobs.next()) {
-			System.out.println(jobs.getInt(1)+"|"+jobs.getString(2));
+	public static void  main(String[] args) {
+		try {
+			database.addNewJob("test job");
+			ResultSet jobs = db.getAllJobs();
+			while(jobs.next()) {
+				System.out.println(jobs.getInt(1)+"|"+jobs.getString(2));
+			}
+		//catching exceptions to test other stuff
+		} catch (SQLException e) {
+			System.out.println(e);
+		} catch (NullPointerException e) {
+			System.out.println(e);
 		}
 	}
 	
