c8ac3ee4845c66adf6aa6f7317eab53c23a6c150
ZiarayZ <jakesmith@binaryrage.com>
2021-05-11 16:41:57 +0100

    setting up main menu

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class UserInterface extends JFrame {
     private LoginUI loginWindow;
     private ManageUsersUI usersWindow;
+    private JPanel contentPane;
     
     public UserInterface(database db, UserManagement user) {
         loginWindow = new LoginUI(user);
         usersWindow = new ManageUsersUI(user, db);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Main Menu");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 534, 363);
+		setContentPane(contentPane);
     }//craft a main menu to default to
 
     public void displayLogin() {
