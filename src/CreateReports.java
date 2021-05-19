9d6e204c4fb9fcf82002c5806735b185e119bbf2
project-kieran <mcdonough_kieran@yahoo.co.uk>
2021-05-19 16:08:30 +0100

    changes to print values instead of memory address

--- src/CreateReports.java
+++ src/CreateReports.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 public class CreateReports {
     private static database tasksDB;
     private dbConnection dbConnect; //variable for report-specific queries
-    private String taskStatus;
-    private String caretakerPerformance;
-    private String taskCompletionLevel;
+    private ResultSet taskStatus;
+    private ResultSet caretakerPerformance;
+    private ResultSet taskCompletionLevel;
 
     public CreateReports(database db) {
         tasksDB = db;
@@ -20,16 +20,18 @@ public class CreateReports {
     }
 
     public void createTaskStatusReport() throws IOException {
-    	ResultSet taskCompletion = dbConnect.RunSQLQuery("SELECT task_id, task_name, completed FROM tasks");
-    	taskStatus = taskCompletion.toString(); //turn db entries into string types
+    	taskStatus = dbConnect.RunSQLQuery("SELECT task_id, task_name, completed FROM tasks");
     	
     	File statusFile = new File("TaskStatus.txt"); //file to be written to
-    	FileWriter fwrite = new FileWriter(statusFile, true);
+    	FileWriter fwrite = new FileWriter(statusFile, false);
     	PrintWriter pwrite = new PrintWriter(fwrite);
     	
     	try {
-    		while (taskCompletion.next()) {
-    			pwrite.println(taskStatus); //prints each task on a new line in text file    			
+    		while (taskStatus.next()) {
+    			pwrite.println("Task ID: " +taskStatus.getString(1)); //print each sql column to file as string
+    			pwrite.println("Task name: "+taskStatus.getString(2));
+    			pwrite.println("Completion Status: "+taskStatus.getString(3));
+    			pwrite.println(" "); 			
     	  }
     	} catch (SQLException e) {
     		System.out.println("Failed to connect "+ e);
@@ -42,17 +44,17 @@ public class CreateReports {
     } //end of task status report
     
     public void createCaretakerReport() throws IOException {
-    	//sql query to display user and number of logged tasks (not tested)
-    	ResultSet caretakerDetails = dbConnect.RunSQLQuery("SELECT user_name, COUNT(logged_id) FROM users LEFT JOIN logged_tasks ON users.user_id = logged_tasks.user_id GROUP BY user_name, user_id");
-    	caretakerPerformance = caretakerDetails.toString();
+    	//sql query to display user and number of logged tasks
+    	caretakerPerformance = dbConnect.RunSQLQuery("SELECT user_name, COUNT(logged_id) FROM users LEFT JOIN logged_tasks ON users.user_id = logged_tasks.user_id GROUP BY user_name, logged_tasks.user_id");
+
     	
     	File caretakerFile = new File("Caretaker.txt");
-    	FileWriter fwrite = new FileWriter(caretakerFile, true);
+    	FileWriter fwrite = new FileWriter(caretakerFile, false);
     	PrintWriter pwrite = new PrintWriter(fwrite);
     	
     	try {
-    		while (caretakerDetails.next()) {
-    			pwrite.println(caretakerPerformance);
+    		while (caretakerPerformance.next()) {
+    			pwrite.println("Caretaker name: " + caretakerPerformance.getString(1)+" "+ "No. of Logged Tasks: "+ caretakerPerformance.getString(2));
     		}
     	} catch (SQLException e) {
     		System.out.println("Failed to connect "+ e);
@@ -65,16 +67,15 @@ public class CreateReports {
     } //end of caretaker report
     
     public void createCompletedTaskReport() throws IOException {
-    	ResultSet completedTasks = tasksDB.getAllLoggedTasks(null); //may need non null argument
-    	taskCompletionLevel = completedTasks.toString();
+    	taskCompletionLevel = tasksDB.getAllLoggedTasks(null); //error to fix here
     	
     	File completedFile = new File("Completed.txt");
-    	FileWriter fwrite = new FileWriter(completedFile, true);
+    	FileWriter fwrite = new FileWriter(completedFile, false);
     	PrintWriter pwrite = new PrintWriter(fwrite);
     	
     	try {
-    		while (completedTasks.next()) {
-    			pwrite.println(taskCompletionLevel);
+    		while (taskCompletionLevel.next()) {
+    			pwrite.println(" "+ taskCompletionLevel.getString(1));//still need to finish this println
     		}
     	} catch (SQLException e) {
     		System.out.println("Failed to connect " + e);
