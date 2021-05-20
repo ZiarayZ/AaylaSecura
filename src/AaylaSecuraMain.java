56d1a89986a1bda683ead4aee07fef90c03d64d4
ZiarayZ <jakesmith@binaryrage.com>
2021-05-20 12:57:07 +0100

    place commented out code back

--- src/AaylaSecuraMain.java
+++ src/AaylaSecuraMain.java
@@ -8,7 +8,7 @@ public class AaylaSecuraMain {
 	private static UserManagement user;
 	private static LogTasks logged_tasks;
 	private static UserInterface window;
-	//private static CreateReports reportCreation;
+	private static CreateReports reportCreation;
 
 	public static void main(String[] args) {
 		//clear and repopulate database
@@ -16,19 +16,19 @@ public class AaylaSecuraMain {
 		//initialise classes
 		logged_tasks = new LogTasks(db);
 		user = new UserManagement(db);
-		//reportCreation = new CreateReports(db);
+		reportCreation = new CreateReports(db);
 		window = new UserInterface(db, user, logged_tasks);
 		//set window frame visible
 		window.setVisible(true);
 
 		//testing
-		/*try {
+		try {
 			reportCreation.createTaskStatusReport();
 			reportCreation.createCaretakerReport();
 			reportCreation.createCompletedTaskReport();
 		//catching exceptions to test other stuff
 		} catch (IOException e) {
 			System.out.println(e);
-		}*/
+		}
 	}
 }
