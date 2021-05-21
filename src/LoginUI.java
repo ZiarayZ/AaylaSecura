bf11e69d0066d1dfa484692d9022a181fcfcc987
ZiarayZ <jakesmith@binaryrage.com>
2021-05-21 14:35:12 +0100

    UI update
    -Color code simplified
    -Add labelled password requirements

--- src/LoginUI.java
+++ src/LoginUI.java
@@ -23,19 +23,28 @@ public class LoginUI extends JPanel {
 	private JPasswordField passwordField;
 	private JPasswordField changePasswordField;
 	private UserManagement User;
+	private JLabel upperLbl;
+	private JLabel lowerLbl;
+	private JLabel numberLbl;
+	private JLabel nonAlphanumericLbl;
 
 	/**
 	 * Create the Pane.
 	 */
 	public LoginUI(UserInterface UI, UserManagement verifyUser) {
+		final Color red = new Color(236, 28, 36);
+		final Color orange = new Color(248, 152, 29);
+		final Color green = new Color(94, 186, 125);
+		final Color grey = new Color(238, 238, 238);
+		final Color white = new Color(255, 255, 255);
 		User = verifyUser;
 		window = UI;
-		setBackground(new Color(255, 255, 255));
+		setBackground(white);
 		setBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(119, 136, 153), new Color(192, 192, 192)));
 		setLayout(new GridBagLayout());
 		JPanel fixedPane = new JPanel();
 		fixedPane.setLayout(null);
-		fixedPane.setBackground(new Color(255, 255, 255));
+		fixedPane.setBackground(white);
 		
 		JLabel lblHeadingLabel = new JLabel("Please Login");
 		lblHeadingLabel.setOpaque(true);
@@ -53,7 +62,7 @@ public class LoginUI extends JPanel {
 				attemptLogin();
 			}
 		});
-		nameField.setBackground(new Color(238, 238, 238));
+		nameField.setBackground(grey);
 		nameField.setBounds(191, 98, 203, 20);
 		fixedPane.add(nameField);
 		nameField.setColumns(10);
@@ -71,7 +80,7 @@ public class LoginUI extends JPanel {
 				attemptLogin();
 			}
 		});
-		passwordField.setBackground(new Color(238, 238, 238));
+		passwordField.setBackground(grey);
 		passwordField.setBounds(191, 148, 202, 20);
 		fixedPane.add(passwordField);
 		
@@ -84,40 +93,70 @@ public class LoginUI extends JPanel {
 		JProgressBar passStrength = new JProgressBar(0, 100);
 		passStrength.setValue(0);
 		passStrength.setBackground(new Color(140, 255, 251));
-		passStrength.setForeground(new Color(236, 28, 36));
+		passStrength.setForeground(red);
 		passStrength.setBounds(90, 293, 375, 25);
 
 		changePasswordField = new JPasswordField();
 		changePasswordField.addKeyListener(new KeyListener() {
 			public void keyTyped(KeyEvent event) {
-				//Do Nothing
+				char key = event.getKeyChar();
+				//check key
+				if (key != KeyEvent.CHAR_UNDEFINED) {
+					if (Character.isDigit(key)) {
+						numberLbl.setForeground(green);
+					}
+					if (Character.isUpperCase(key)) {
+						upperLbl.setForeground(green);
+					}
+					if (Character.isLowerCase(key)) {
+						lowerLbl.setForeground(green);
+					}
+					if (!Character.isDigit(key) && !Character.isUpperCase(key) && !Character.isLowerCase(key)) {
+						nonAlphanumericLbl.setForeground(green);
+					}
+				}
 			}
 			public void keyPressed(KeyEvent event) {
 				//Press Enter
-				int key = event.getKeyCode();
-				if (key == KeyEvent.VK_ENTER) {
+				if (event.getKeyCode() == KeyEvent.VK_ENTER) {
 					attemptPassChange();
 				}
 			}
 			//once a key is pressed then released will test the password's strength
 			public void keyReleased(KeyEvent event) {
-				int strength = (int)UserManagement.passwordStrength(changePasswordField.getPassword());
+				char[] newPass = changePasswordField.getPassword();
+				if (event.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+					String result = UserManagement.checkPassword(newPass);
+					if (result.contains("U")) {
+						upperLbl.setForeground(red);
+					}
+					if (result.contains("L")) {
+						lowerLbl.setForeground(red);
+					}
+					if (result.contains("D")) {
+						numberLbl.setForeground(red);
+					}
+					if (result.contains("N")) {
+						nonAlphanumericLbl.setForeground(red);
+					}
+				}
+				int strength = (int)UserManagement.passwordStrength(newPass);
 				if (strength >= 100) {
 					passStrength.setValue(100);
-					passStrength.setForeground(new Color(94, 186, 125));
+					passStrength.setForeground(green);
 				} else if (strength >= 60) {
 					passStrength.setValue(strength);
-					passStrength.setForeground(new Color(94, 186, 125));
+					passStrength.setForeground(green);
 				} else if (strength >= 40) {
 					passStrength.setValue(strength);
-					passStrength.setForeground(new Color(248, 152, 29));
+					passStrength.setForeground(orange);
 				} else if (strength < 40) {
 					passStrength.setValue(strength);
-					passStrength.setForeground(new Color(236, 28, 36));
+					passStrength.setForeground(red);
 				}
 			}
 		});
-		changePasswordField.setBackground(new Color(238, 238, 238));
+		changePasswordField.setBackground(grey);
 		changePasswordField.setBounds(191, 198, 202, 20);
 		fixedPane.add(changePasswordField);
 		
@@ -144,6 +183,24 @@ public class LoginUI extends JPanel {
 		btnUpdatePasswordButton.setBounds(300, 236, 157, 37);
 		fixedPane.add(btnUpdatePasswordButton);
 		fixedPane.add(passStrength);
+
+		//change password labels
+		upperLbl = new JLabel("Upper case: A-Z");
+		upperLbl.setForeground(red);
+		upperLbl.setBounds(465, 101, 90, 14);
+		lowerLbl = new JLabel("Lower case: a-z");
+		lowerLbl.setForeground(red);
+		lowerLbl.setBounds(465, 151, 90, 14);
+		numberLbl = new JLabel("Numbers: 0-9");
+		numberLbl.setForeground(red);
+		numberLbl.setBounds(465, 201, 90, 14);
+		nonAlphanumericLbl = new JLabel("Symbols: ~!@#$%^&*()_-");
+		nonAlphanumericLbl.setForeground(red);
+		nonAlphanumericLbl.setBounds(465, 251, 90, 14);
+		fixedPane.add(upperLbl);
+		fixedPane.add(lowerLbl);
+		fixedPane.add(numberLbl);
+		fixedPane.add(nonAlphanumericLbl);
 		fixedPane.setPreferredSize(new Dimension(555, 329));
 		add(fixedPane);
 	}
