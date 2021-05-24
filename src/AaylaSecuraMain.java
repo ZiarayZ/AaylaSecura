a571e91da1129833ae6b89034d7bbe50cdc762cd
ZiarayZ <jakesmith@binaryrage.com>
2021-05-24 05:24:53 +0100

    comment out dbreset

--- src/AaylaSecuraMain.java
+++ src/AaylaSecuraMain.java
@@ -1,7 +1,7 @@
 public class AaylaSecuraMain {
 	
 	static database db = new database();
-	private static databaseReset dbReset = new databaseReset(db);
+	private static databaseReset dbReset = new databaseReset(db);//for testing
 	private static UserManagement user;
 	private static taskEntry myTE;
 	private static taskAssign myTA;
@@ -10,7 +10,7 @@ public class AaylaSecuraMain {
 
 	public static void main(String[] args) {
 		//clear and repopulate database
-		dbReset.reset();
+		//dbReset.reset();//resetting database to factory settings for testing
 		//initialise classes
 		myTE = new taskEntry(db);
 		myTA = new taskAssign(db);
